de01314878b0283695b0b9557b92c7ec531d9413
RWK <rawagnerkaiser@astro.ufl.edu>
2015-02-28 15:26:51 -0800

    First Local Commit

---
+++ HessCMD.py
@@ -0,0 +1,85 @@
+### Built with Python 2.6
+
+### Import other packages
+from pylab import *
+from numpy import *
+from random import *
+from scipy import *
+from matplotlib import *
+rcdefaults()
+matplotlib.rc('font',family='Bitstream Vera Serif')
+
+
+
+def plotHess(mag, color, levels='None', xlims=[-1,3], ylims=[28,20], colormap=cm.gray_r cbarr='None', cbarrtitle='', xlabel='', ylabel='', saveas='HessCMD.png'):
+    """
+    plotHess(mag,color) is a function intended to create a contour plot of stellar density in the canonical Color-Magnitude Diagram (CMD). plotHess() plots all the stars in the CMD and then overplots a contour of their density.
+
+    mag = magnitudes of the stars
+    color = colors of the stars
+    mag and color are required to run plotHess() and need to be the same length arrays.
+
+
+    Optional keyword arguments:
+    
+    =========   =======================================================
+    Keyword     Description
+    =========   =======================================================
+    levels:    levels to be used for density contour; if 'None', the defaults are defined by the contour() function, which may not be optimal for the users dataset and may be changed if necessary.
+    ylims:      define the y-range of the plotted values; defaults will need to be changed to fit user's data.
+    xlims:      define the x-range of the plotted values; defaults will need to be changed to fit user's data.
+    colormap:   allows the user to choose a Python color map to use for the contour; the default is grayscale.
+    cbarr:      'None' means the colorbar will not be plot as a default. To change this, set cbarr='Yes'; in a true Hess diagram, the cbarr values represent the stellar point density in the CMD plot.
+    cbarrtitle: sets the title for the colorbar; should be string
+    xlabel:     sets the xlabel for the plot; should be string
+    ylabel:     sets the ylabel for the plot; should be string
+    saveas:     pathway for saving the output plot. The default is to save in the same folder as "HessCMD.png"
+    
+    
+    """
+    ### Set figure options
+    fig = pylab.figure(1, figsize=(8, 7))
+    
+    ### Plot all stars in CMD as small points
+    plot(color,mag,'ko',ms=0.2,zorder=1)
+    
+    ### Use color and magnitude data to split and average into a binned 2D histogram
+    Z, xedges, yedges = np.histogram2d(color,mag,bins=(300,300))
+    
+    ### Find the cell centers from the cell edges
+    x = 0.5*(xedges[:-1] + xedges[1:])
+    y = 0.5*(yedges[:-1] + yedges[1:])
+    
+    ### Promote to 2D arrays
+    Y, X = np.meshgrid(y, x)
+    
+    ### Mask out values without data
+    Z = np.ma.array(Z, mask=(Z==0))
+    
+    ### Set levels options as default or user-defined option
+    if levels=='None':
+        cntr=plt.contourf(X,Y,Z,cmap=cm.gray_r,zorder=2)
+    else:
+        cntr=plt.contourf(X,Y,Z,levels=levels,cmap=colormap,zorder=2)
+    cntr.cmap.set_under('white')
+    
+    ### Set plot limits as default or user-defined option
+    xlim(xlims[0],xlims[1]);ylim(ylims[0],ylims[1])
+    
+    ### Determine whether to plot a color bar
+    if cbarr!='None':
+        cbar_ax = fig.add_axes([0.91, 0.14, 0.02, 0.7])
+        d=fig.colorbar(cntr, cax=cbar_ax, title=cbarrtitle)
+        d.set_label(label='Relative Point Density',size=10)
+
+    ### Set final options and save figure to default (current folder) or to a user-defined location on disk
+    ylabel(str(ylabel))
+    xlabel(str(xlabel))
+    savefig(saveas,dpi=300)
+    plt.show()
+    
+    return
+
+
+
+
---
+++ HessExample.py
@@ -0,0 +1,23 @@
+from pylab import *
+from numpy import *
+from random import *
+from scipy import *
+from matplotlib import *
+rcdefaults()
+matplotlib.rc('font',family='Bitstream Vera Serif')
+
+import HessCMD
+
+#########  Example  #########
+
+### load in data and define color and magnitude
+viall=loadtxt('photometry.txt') # example photometry
+mag1=viall[:,0]  #606 (~V)
+mag2=viall[:,1]  #814 (~I)
+color=mag1-mag2  # F606W - F814W (~(V-I) color)
+
+
+#### run plotHess() on magnitude and color:
+# utilize options to: define levels, save as a particular file, include a colorbar, set labels, etc.
+HessCMD.plotHess(mag2, color, levels=arange(25,225,25), cbarrtitle='Density', xlabel='F606W $-$ F814W (mag)', ylabel='F814W (mag)', saveas='HessCMD_withoptions.png', cbarr='Yes')
+
